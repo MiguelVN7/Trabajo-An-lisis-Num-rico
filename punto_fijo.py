27b31d884d45ebbfd0f03dd987da2a3c735b4b53
saradrl <67977092+saradrl@users.noreply.github.com>
2024-03-18 22:20:10 -0500

    Add files via upload

--- punto_fijo.py
+++ punto_fijo.py
@@ -1,41 +1,62 @@
+# Algoritmo de punto fijo
+# [a,b] intervalo de búsqueda
+# error = tolera
 
-#! Entre mas alto sea el k mas se demora en converger el punto fijo 
+import numpy as np
 
-def Punto_fijo(x0, g, tol, Niter):
-    g = g(x0)
-    E = abs(x0 - g)
-    iter = 0
-
-    while (iter <= Niter and E >= tol):
-        x0 = g
-        g = g(x0)
-        E = abs(x0 - g)
-        iter += 1
-    if E < tol:
-        print("g es raiz con tolerancia optimista")
-
-    else:
-        print("maximo numero de iteraciones alcanzado")
+def puntofijo(gx,a,tolera, iteramax = 15):
+    i = 1 # iteración
+    b = gx(a)
+    tramo = abs(b-a)
+    while(tramo>=tolera and i<=iteramax ):
+        a = b
+        b = gx(a)
+        tramo = abs(b-a)
+        i = i + 1
+    respuesta = b
     
-    error = E
-    iteration = iter
-    x = x0
+    # Validar respuesta
+    if (i>=iteramax ):
+        respuesta = np.nan
+    return(respuesta)
 
-    return error, iteration, x
+# PROGRAMA ---------
 
-x0 = 1
-tol = 0.0001
-Niter = 100
+# INGRESO (esta es la función que podemos cambiar)
+fx = lambda x: np.exp(-x) - x
+gx = lambda x: np.exp(-x)
 
-#! Funcion de ejemplo
-def Example_function(x):
-    return (x-1) * (x-2)
+a = 0       # intervalo
+b = 1
+tolera = 0.001
+iteramax = 15  # itera máximo
+muestras = 51  # gráfico
+tramos = 50
 
-#? Ejecucion
+# PROCEDIMIENTO
+respuesta = puntofijo(gx,a,tolera)
 
-error, iteration ,x = Punto_fijo(x0, Example_function, tol, Niter)
+# SALIDA
+print(respuesta)
 
-print("Solucion: ", x) 
-print("numero de iteraciones: ", iteration)
-print("Error: ", error)
+# GRAFICA
+# calcula los puntos para fx y gx
+xi = np.linspace(a,b,muestras)
+fi = fx(xi)
+gi = gx(xi)
+yi = xi
 
+import matplotlib.pyplot as plt
+
+plt.plot(xi,fi, label='f(x)')
+plt.plot(xi,gi, label='g(x)')
+plt.plot(xi,yi, label='y=x')
+
+if (respuesta!= np.nan):
+    plt.axvline(respuesta)
+plt.axhline(0, color='k')
+plt.title('Punto Fijo')
+plt.legend()
+plt.show()
+
+#"C:\Users\sarar\OneDrive\Escritorio\punto_fijo.py"
